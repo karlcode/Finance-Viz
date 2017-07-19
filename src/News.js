c1613693ba294de77dd7c51fc4ffe4a4b86479d4
karlcode <karl.l1@hotmail.com>
2017-07-19 20:22:00 +1000

    Iterate and render json result

--- src/News.js
+++ src/News.js
@@ -6,12 +6,16 @@ var myHeaders = new Headers({
 });
 
 export default class News extends Component {
+    
 state = {
+    series: [{
     title: 'Nothing yet',
     date: '',
+    }]
   }
 grabNews(){
-    const {title, author} = this.state;
+    var result = []
+    const {title, date} = this.state;
     var username = "62f863e182736402fd729bc2dcdfa160";
     var password = "e7ae8da36d32633febfac0b6b295b45b";
     var auth = "Basic " + new Buffer(username + ':' + password).toString('base64');
@@ -24,21 +28,21 @@ grabNews(){
     fetch(url, myInit)
     .then(res => res.json())
     .then(response =>{
-        var arr = response
-        for (var i=0;i<arr.length;i++){
-        var tit = arr.data[0].title
-        var y = arr[i][4]
+        for (var i=0; i<response.data.length; i++){
+        var title = response.data[i].title
+        var date = response.data[i].publication_date
+        result.push({title, date})
         }
-    this.setState({title: response.data[0].title,
-                    date: response.data[0].publication_date})
-        console.log(response)
-        console.log(response.data[0].title)
+        console.log(result)
+        result.forEach(key => this.setState({title: key.title, 
+                                            date: key.date}))
+ 
     })
 }
-componentDidMount(){
-    this.grabNews()
+    componentDidMount(){
+        this.grabNews()
 
-}
+    }
 
 render() {
 const {ticker} = this.props;
